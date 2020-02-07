7fe80316c7b6782634bddfe69b326acf90ccbd77
friedcelery <38055253+friedcelery@users.noreply.github.com>
2020-02-06 16:23:28 -0800

    implemented volume slider

--- fmSynth/Source/PluginEditor.cpp
+++ fmSynth/Source/PluginEditor.cpp
@@ -18,6 +18,24 @@ FmSynthAudioProcessorEditor::FmSynthAudioProcessorEditor (FmSynthAudioProcessor&
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+        // these define the parameters of our slider object
+    midiVolume.setSliderStyle (Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle (Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled (true, false, this);
+    midiVolume.setTextValueSuffix (" Volume");
+    midiVolume.setValue(1.0);
+ 
+    // this function adds the slider to the editor
+    addAndMakeVisible (&midiVolume);
+    midiVolume.addListener(this);
+
+}
+
+void FmSynthAudioProcessorEditor::sliderValueChanged (Slider* slider)
+{
+    processor.noteOnVel = midiVolume.getValue();
 }
 
 FmSynthAudioProcessorEditor::~FmSynthAudioProcessorEditor()
@@ -32,11 +50,12 @@ void FmSynthAudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    g.drawFittedText ("Midi Volume",0,0, getWidth(),30, Justification::centred, 1);
 }
 
 void FmSynthAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    midiVolume.setBounds (40, 30, 20, getHeight() - 60);
 }
--- fmSynth/Source/PluginEditor.h
+++ fmSynth/Source/PluginEditor.h
@@ -16,7 +16,7 @@
 //==============================================================================
 /**
 */
-class FmSynthAudioProcessorEditor  : public AudioProcessorEditor
+class FmSynthAudioProcessorEditor  : public AudioProcessorEditor, private Slider::Listener
 {
 public:
     FmSynthAudioProcessorEditor (FmSynthAudioProcessor&);
@@ -27,9 +27,11 @@ public:
     void resized() override;
 
 private:
+
+    void sliderValueChanged (Slider* slider) override;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     FmSynthAudioProcessor& processor;
-
+    Slider midiVolume;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FmSynthAudioProcessorEditor)
 };
