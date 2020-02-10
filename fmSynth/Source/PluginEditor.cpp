47bd01ed4d41a8a0aa3641002121e08417519b7e
friedcelery <38055253+friedcelery@users.noreply.github.com>
2020-02-10 01:15:22 -0800

    Revert "Merge branch 'torrancecui' of github.com:ucsb-cs48-w20/fmSynthesizer into friedcelery"
    This reverts commit 6123d15360dffc15f7427cb330f0766f14827ecb, reversing
    changes made to 32874369f510d4db5eb3e83b2b9296ab04c5ee16.

--- fmSynth/Source/PluginEditor.cpp
+++ fmSynth/Source/PluginEditor.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
   ==============================================================================
 
@@ -85,66 +84,3 @@ void FmSynthAudioProcessorEditor::resized()
     cutoffSlider.setBounds(300, 50, 20, 100);
 >>>>>>> 6c63939af4a695d4d1d8df3503ac15ca1888cc08
 }
-=======
-/*
-  ==============================================================================
-    This file was auto-generated!
-    It contains the basic framework code for a JUCE plugin editor.
-  ==============================================================================
-*/
-
-#include "PluginProcessor.h"
-#include "PluginEditor.h"
-
-//==============================================================================
-FmSynthAudioProcessorEditor::FmSynthAudioProcessorEditor (FmSynthAudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
-{
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (200, 200);
-    
-    //This defines the parameters of our slider object.
-    gainSlider.setSliderStyle(Slider::LinearBarVertical);
-    gainSlider.setRange(0.0f, 1.0f, 0.01f);
-    gainSlider.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-    gainSlider.setPopupDisplayEnabled(true, false, this);
-    gainSlider.setTextValueSuffix (" Volume");
-    gainSlider.setValue(0.25);
-    
-    // this function adds the slider to the editor
-    addAndMakeVisible (&gainSlider);
-    
-    //Add gainSlider listener.
-    gainSlider.addListener(this);
-    
-}
-
-FmSynthAudioProcessorEditor::~FmSynthAudioProcessorEditor()
-{
-}
-
-//==============================================================================
-void FmSynthAudioProcessorEditor::paint (Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setColour (Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Volume Slider Test: SineWave", getLocalBounds(), Justification::centred, 1);
-}
-
-void FmSynthAudioProcessorEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-    // sets the position and size of the slider with arguments (x, y, width, height)
-    gainSlider.setBounds (40, 30, 20, getHeight() - 60);
-}
-
-void FmSynthAudioProcessorEditor::sliderValueChanged(Slider* slider)
-{
-    processor.gain = gainSlider.getValue();
-}
->>>>>>> bea4b0889bf5cdafda43e64deeab48c4e81c0d8a
--- fmSynth/Source/PluginProcessor.h
+++ fmSynth/Source/PluginProcessor.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
   ==============================================================================
 
@@ -70,69 +69,3 @@ private:
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FmSynthAudioProcessor)
 };
-=======
-/*
-  ==============================================================================
-    This file was auto-generated!
-    It contains the basic framework code for a JUCE plugin processor.
-  ==============================================================================
-*/
-
-#pragma once
-
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "Oscillator.h"
-
-//==============================================================================
-/**
-*/
-class FmSynthAudioProcessor  : public AudioProcessor
-{
-public:
-    //================Variables====================================================
-    float gain;
-    float noteOnVel;
-    //==============================================================================
-    FmSynthAudioProcessor();
-    ~FmSynthAudioProcessor();
-
-    //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
-
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
-
-    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
-
-    //==============================================================================
-    AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
-
-    //==============================================================================
-    const String getName() const override;
-
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const String getProgramName (int index) override;
-    void changeProgramName (int index, const String& newName) override;
-
-    //==============================================================================
-    void getStateInformation (MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
-
-private:
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FmSynthAudioProcessor)
-    Random random;
-    Sine sine;
-};
->>>>>>> bea4b0889bf5cdafda43e64deeab48c4e81c0d8a
