0f9c993796c6909c379ada6c868f7e67a44e8427
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2020-02-12 19:03:54 -0800

    h weirdness

--- fmSynth/Source/SawOsc.h
+++ fmSynth/Source/SawOsc.h
@@ -1,5 +1,4 @@
 //
-//  SawOsc.h
 //  SawOsc.hpp
 //  fmSynth
 //
@@ -11,10 +10,6 @@ struct SawSound : public SynthesiserSound
 {
 
     SawSound() {}
-    
-    bool appliesToNote    (int) override        { return true; }
-    bool appliesToChannel (int) override        { return true; }
-    
 
     bool appliesToNote(int) override { return true; }
     bool appliesToChannel(int) override { return true; }
@@ -26,27 +21,11 @@ class SawVoice : public SynthesiserVoice
 public:
 
     SawVoice() {}
-    
-    bool canPlaySound (SynthesiserSound* sound) override
 
     bool canPlaySound(SynthesiserSound* sound) override
     {
         return dynamic_cast<SawSound*> (sound) != nullptr;
     }
-    
-//    DUMMY FUNCTION
-//    void setParam1(float* param1);
-    
-    void startNote (int midiNoteNumber, float velocity,
-                    SynthesiserSound* sound, int /*currentPitchWheelPosition*/) override;
-    
-    void stopNote (float /*velocity*/, bool allowTailOff) override;
-    
-    void pitchWheelMoved (int) override      {}
-    void controllerMoved (int, int) override {}
-    
-    void renderNextBlock (AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
-    
 
     //    DUMMY FUNCTION
     //    void setParam1(float* param1);
--- fmSynth/Source/SineOsc.h
+++ fmSynth/Source/SineOsc.h
@@ -15,12 +15,6 @@
 
 struct SineSound : public SynthesiserSound
 {
-    
-    SineSound() {}    
-    
-    bool appliesToNote    (int) override        { return true; }
-    bool appliesToChannel (int) override        { return true; }
-    
 
     SineSound() {}
 
@@ -34,27 +28,11 @@ class SineVoice : public SynthesiserVoice
 public:
 
     SineVoice() {}
-    
-    bool canPlaySound (SynthesiserSound* sound) override
 
     bool canPlaySound(SynthesiserSound* sound) override
     {
         return dynamic_cast<SineSound*> (sound) != nullptr;
     }
-    
-//    DUMMY FUNCTION 
-//    void setParam1(float* param1);
-    
-    void startNote (int midiNoteNumber, float velocity,
-                    SynthesiserSound* sound, int /*currentPitchWheelPosition*/) override;
-    
-    void stopNote (float /*velocity*/, bool allowTailOff) override;
-    
-    void pitchWheelMoved (int) override      {}
-    void controllerMoved (int, int) override {}
-    
-    void renderNextBlock (AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
-    
 
     //    DUMMY FUNCTION 
     //    void setParam1(float* param1);
@@ -70,4 +48,9 @@ public:
     void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
 
 private:
+    void angleCap(); // call after you increment currentAngle to avoid overflows
+
+    float currentAngle = 0.0, angleDelta = 0.0,
+        level = 0.0, tailOff = 0.0, twoPi = 0.0;
+
 };
